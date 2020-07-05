a008b31aef2b39f1e7ede8282b33fb20924f493d
BuildTools <unconfigured@null.spigotmc.org>
2020-07-05 12:02:46 +0200

    update tokens

--- src/index.js
+++ src/index.js
@@ -5,23 +5,20 @@ const dayjs = require('dayjs')
 const cron = require('node-cron');
 
 cron.schedule('0 1 * * *', () => {
-    
-   const date = dayjs().subtract(5, 'd').format('YYYY-MM-DD')
-   linky.config({
-    warp10: {
-        w10URL: conf.warp10.w10URL,
-        rt: conf.warp10.rt,
-        wt: conf.warp10.wt,
-    },
-    enedis: {
-        login: conf.enedis.login,
-        passwd: conf.enedis.password,
-        pdl: conf.enedis.pdl
-    },
-}).updateHistory(date, 1)
-    .then(r => {
-        console.log(r)
-    });
-  });
-  
-WebServer.start();
+
+    const date = dayjs().subtract(5, 'd').format('YYYY-MM-DD')
+    linky.config({
+        warp10: {
+            w10URL: conf.warp10.w10URL,
+            rt: conf.warp10.rt,
+            wt: conf.warp10.wt,
+        },
+        enedis: {
+            login: conf.enedis.login,
+            passwd: conf.enedis.password,
+            pdl: conf.enedis.pdl
+        },
+    }).updateHistory(date, 1).then(r => console.log(r));
+});
+
+// WebServer.start();
